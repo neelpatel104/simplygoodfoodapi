8ba83ce9db6652455d154a4bd0dd598a7e571b1e
neelpatel104 <63991953+neelpatel104@users.noreply.github.com>
2021-11-29 14:14:35 -0500

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -128,7 +128,7 @@ router.post('/login', async (req,res)=> {
                     response.data.password = undefined;
                     // Generate Token and send cookie with the token
                     let token = jwt.generateAccessToken(response.data);
-                    res.status(token.status).cookie('token', token.data, {path:'/', secure: true, httpOnly: true, sameSite: false}).send({
+                    res.status(token.status).cookie('token', token.data, {path:'/', secure: true, httpOnly: true, sameSite: "none"}).send({
                         message: "Login Successful!",
                         response
                     });
